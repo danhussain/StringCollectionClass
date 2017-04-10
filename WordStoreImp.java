dbc84db2aa526aaba8ae383d47adba350deb298b
Syed Danyel Hussain <syeddanyelhussain@outlook.com>
2017-04-10 12:25:05 +0100

    Add files via upload

---
+++ WordStoreImp.java
@@ -0,0 +1,24 @@
+public class WordStoreImp implements WordStore
+{
+    private int numberOfStrings;
+    private int numberOfOccurences;
+    public WordStoreImp(int nOS)
+    {
+        
+    }
+    
+    public void add(String word)
+    {
+        return;
+    }
+    
+    public int count(String word)
+    {
+        return -1;
+    }
+        
+    public void remove(String word)
+    {
+        return;
+    }
+}
